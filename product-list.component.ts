4cfba7bd9d963b3e25b2b3fd4a4cdddf6f01ce48
Altynbek <78595870+altynbek01@users.noreply.github.com>
2021-03-03 15:03:59 +0600

    Add files via upload

--- product-list.component.ts
+++ product-list.component.ts
@@ -1,6 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 import { products } from "../products";
+import { category } from "../category";
+import { Task } from "../Task";
 
 @Component({
   selector: "app-product-list",
@@ -8,7 +10,45 @@ import { products } from "../products";
   styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent {
+  @Output() remove = new EventEmitter();
   products = products;
+  category = category;
+  Task = Task;
+  id: number;
+  flag: boolean;
+  idInOneCategory: number;
+  numberOfLikes: number;
+  numberOfDisLikes: number;
+  newTasks: Task[];
+  doneTasks: Task[];
+  currentTask: Task;
+
+  constructor() {
+    this.flag = false;
+    this.numberOfLikes = 0;
+    this.numberOfDisLikes = 0;
+    this.newTasks = [];
+    this.doneTasks = [];
+    this.currentTask = new Task();
+  }
+  start() {
+    this.currentTask.id = this.newTasks.length + 1;
+    this.newTasks.push(this.currentTask);
+    this.currentTask = new Task();
+  }
+
+  clickButton(id: number) {
+    this.flag = !this.flag;
+    this.id = id;
+    return id;
+  }
+  likeButtonClick(id: number) {
+    this.products[id].rating++;
+  }
+  disLikeButtonClick(id: number) {}
+  removeProduct(index: number) {
+    this.products.splice(index, 1);
+  }
 
   share() {
     //window.alert("The product has been shared!");
